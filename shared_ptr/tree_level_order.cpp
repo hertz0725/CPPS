18ddb660d1267b8a353877682e9e1a04fbaf7755
hertz0725 <hertz0725@hotmail.com>
2024-08-15 23:52:53 +0800

    Create tree_level_order.cpp

---
+++ shared_ptr/tree_level_order.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (!root) {
+            return {};
+        }
+
+        std::queue<TreeNode*> tq;
+        tq.push(root);
+        auto last = tq.back();
+        
+        std::deque<int> level_dq;
+        bool seq_order = true;
+
+        std::vector<std::vector<int>> res_vec;
+
+        while (!tq.empty()) {
+            auto out = tq.front();
+            tq.pop();
+            
+            if (seq_order) {
+                level_dq.push_back(out->val);
+            } else {
+                level_dq.push_front(out->val);
+            }
+
+            if (out->left) {
+                tq.push(out->left);
+            }
+            if (out->right) {
+                tq.push(out->right);
+            }
+
+            if (out == last) {
+                res_vec.push_back(std::vector<int>(level_dq.begin(), level_dq.end()));
+                seq_order = !seq_order;
+                level_dq.clear();
+
+                last = tq.back();
+            }
+        }
+
+        return res_vec;
+    }
+};
